fcd2e4153a4511db5d4111caa3d4ab8f7e5d9668
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-08-25 22:38:56 -0500

    setup firebase hosting

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    useEmulators: true,
+    firebase: {
+        apiKey: "AIzaSyDCIrvCo0Ql25ZrOJy_NvgX8sXhITbeg2w",
+        authDomain: "todos-f2f1e.firebaseapp.com",
+        projectId: "todos-f2f1e",
+        storageBucket: "todos-f2f1e.appspot.com",
+        messagingSenderId: "634079614760",
+        appId: "1:634079614760:web:a670a236ed485ef28de21d"
+    }
+}
