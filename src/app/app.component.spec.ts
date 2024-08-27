00880bc217307fd5631f7a2c6d743e49bfb8615f
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-08-26 21:51:28 -0500

    add unit tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,23 @@
-import { TestBed } from '@angular/core/testing';
-import { Component } from '@angular/core';
+import { MockBuilder, MockedComponentFixture, MockRender, ngMocks } from 'ng-mocks';
+import { RouterOutlet } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LogoutComponent } from './auth';
 
-@Component({
-  standalone: true,
-  selector: 'app-auth-logout',
-  template: ''
-})
-class MockAuthLogout {}
+describe(AppComponent.name, () => {
+  let fixture: MockedComponentFixture<AppComponent>; 
 
-describe('AppComponent', () => {
   beforeEach(async () => {
-    TestBed.overrideComponent(AppComponent, {
-      remove: { imports: [LogoutComponent] },
-      add: { imports: [MockAuthLogout] }
-    })
-
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+    await  MockBuilder(AppComponent);
+  
+    fixture = MockRender(AppComponent);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('show the app-auth-logout component', () => {
+    expect(ngMocks.find(LogoutComponent)).toBeDefined();
+  });
+
+  it('show the router-outlet component', () => {
+    expect(ngMocks.find(RouterOutlet)).toBeDefined();
   });
 });
